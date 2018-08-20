97b96c33e71b4fe4e759c1c60f9d22d51ca94e40
bcoletta.16 <bcoletta.16@westminster-mo.edu>
2018-08-20 14:04:54 -0500

    initial working site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ResumeSectionComponent } from './resume/resume-section/resume-section.component';
+import { ResumeComponent } from './resume/resume.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ResumeSectionComponent,
+    ResumeComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule
